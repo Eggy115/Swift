54ac39ee968060b0f4e44ea4b4c0b995942fc18f
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-02 17:55:32 +0100

    Create maths.swift

---
+++ maths.swift
@@ -0,0 +1,11 @@
+let a = 5
+let b = 10
+let sum = a + b
+let diff = a - b
+let prod = a * b
+let quot = a / b
+
+print("Sum: \(sum)")
+print("Difference: \(diff)")
+print("Product: \(prod)")
+print("Quotient: \(quot)")
